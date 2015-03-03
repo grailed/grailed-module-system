425778722ba661848d6a56fb3e25b5bf2bafff79
Glen Arrowsmith <glen.arrowsmith@gmail.com>
2015-03-03 15:57:02 +1000

    404 page responds with text & json when requested

--- src/module/controller/error.js
+++ src/module/controller/error.js
@@ -1,5 +1,15 @@
 module.exports = {
 	404: function ( _req, _res, _next ) {
-		_res.status( 404 ).render( '404.ejs' );
+		switch ( _req.headers.accept ) {
+		case 'application/json':
+			_res.status( 404 ).send( '{"error":{"message":"Page not found. ¯\\_(ʘ_ʘ)_/¯","status":404}}' );
+			break;
+		case 'text/plain':
+			_res.status( 404 ).send( 'Page not found. ¯\\_(ʘ_ʘ)_/¯' );
+			break;
+		default:
+			_res.status( 404 ).render( '404.ejs' );
+
+		}
 	}
 };
