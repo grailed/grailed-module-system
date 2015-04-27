a969e4e04c5f8494adbfcd227cf12b40df3595cd
Glen Arrowsmith <glen.arrowsmith@gmail.com>
2015-04-27 16:54:23 +1000

    Adds option parameter to error service

--- src/module/service/error.js
+++ src/module/service/error.js
@@ -1,9 +1,10 @@
-module.exports = function ( _status, _message ) {
+module.exports = function ( _status, _message, _options ) {
 	var cast = require( 'sc-cast' ),
 		is = require( 'sc-is' ),
 		error = new Error( cast( _message, 'string', 'An unknown error occurred.' ) );
 
 	if ( is.a.number( _status ) ) error.status = _status;
+	if ( _options ) error.options = _options;
 
 	return error;
 };
